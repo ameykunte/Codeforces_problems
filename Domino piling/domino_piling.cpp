d7fd8c6b7748a108f2e59fd18597d806dc28dace
Amey <amey.k@research.iiit.ac.in>
2023-05-11 02:43:03 +0530

    domino piling

---
+++ Domino piling/domino_piling.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/50/A#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int n, m, a;
+    cin >> n >> m;
+    a = n * m / 2;
+    cout << a << endl;
+    return 0;
+}
