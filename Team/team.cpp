2b92ca317c7c115f14d68259a13b145e41031066
Amey <amey.k@research.iiit.ac.in>
2023-05-11 01:40:46 +0530

    team

---
+++ Team/team.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/231/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b + c > 1)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
