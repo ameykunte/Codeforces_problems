b4a93d5069cae6265344e735f9d0f29904b3d30a
Amey <amey.k@research.iiit.ac.in>
2023-05-14 05:42:19 +0530

    Wrong Subtraction

---
+++ Wrong Subtraction/wrong_subtraction.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/977/A
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    while (k)
+    {
+        if (n % 10 == 0)
+        {
+            n = n / 10;
+        }
+        else
+        {
+            n--;
+        }
+        k--;
+    }
+    cout << n;
+}
