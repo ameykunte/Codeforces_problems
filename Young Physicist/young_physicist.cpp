0744e88de5905384ad0a932001ddd2aa205a2d51
Amey <amey.k@research.iiit.ac.in>
2023-06-10 22:21:30 +0530

    Young Physicist

---
+++ Young Physicist/young_physicist.cpp
@@ -0,0 +1,30 @@
+// https://codeforces.com/problemset/problem/69/A
+#include <cmath>
+#include <iostream>
+#include <cctype>
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int x = 0, y = 0, z = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        x += a;
+        y += b;
+        z += c;
+    }
+    if (x == 0 && y == 0 && z == 0)
+    {
+        cout << "YES"<<endl;
+    }
+    else
+    {
+        cout << "NO"<<endl;
+    }
+}
